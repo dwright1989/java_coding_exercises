f439d14f631246bb0f8dbd1631b4afe014892869
dwright1989 <debbiewright@live.co.uk>
2023-01-17 13:43:37 +0000

    Exercise one complete

--- src/main/java/com/techreturners/exercise001/Exercise001.java
+++ src/main/java/com/techreturners/exercise001/Exercise001.java
@@ -1,30 +1,36 @@
 package com.techreturners.exercise001;
 
+import java.text.DecimalFormat;
 import java.util.List;
 
 public class Exercise001 {
     public String capitalizeWord(String word) {
-        // Add your code here
-        return "";
+        return word.substring(0,1).toUpperCase() + word.substring(1);
     }
 
     public String generateInitials(String firstName, String lastName) {
-        // Add your code here
-        return "";
+        return firstName.charAt(0) + "." + lastName.charAt(0);
     }
 
     public double addVat(double originalPrice, double vatRate) {
-        // Add your code here
-        return 0.0;
+        double vatAmount = originalPrice*(vatRate/100);
+        return Math.round((originalPrice+vatAmount)*100.0)/100.0;
     }
 
     public String reverse(String sentence) {
-        // Add your code here
-        return "";
+        if(sentence.isEmpty())
+            return sentence;
+        return reverse(sentence.substring(1)) + sentence.charAt(0);
     }
 
+
     public int countLinuxUsers(List<User> users) {
-        // Add your code here
-        return 0;
+        int linuxUserCount = 0;
+        for(User user: users){
+            if(user.getType()=="Linux"){
+                linuxUserCount++;
+            }
+        }
+        return linuxUserCount;
     }
 }
