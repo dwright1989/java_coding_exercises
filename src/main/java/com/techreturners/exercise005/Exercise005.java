85bc14d5cc2f6464f551539618569a88649fc520
dwright1989 <debbiewright@live.co.uk>
2023-01-18 14:47:22 +0000

    Exercise five complete

--- src/main/java/com/techreturners/exercise005/Exercise005.java
+++ src/main/java/com/techreturners/exercise005/Exercise005.java
@@ -2,8 +2,20 @@ package com.techreturners.exercise005;
 
 public class Exercise005 {
 
-    public boolean isPangram(String input) {
-        throw new UnsupportedOperationException("You can delete this statement and add your code here.");
+     public boolean isPangram(String input) {
+        boolean isPangram = false;
+        input = input.toLowerCase();
+        String alphabet = "";
+         for(char i='a';i<='z';i++) {
+            if(input.indexOf(i)!=-1){
+                alphabet+=i;
+            }
+         }
+
+         if(alphabet.length()==26){
+             isPangram = true;
+         }
+        return isPangram;
     }
 
 }
