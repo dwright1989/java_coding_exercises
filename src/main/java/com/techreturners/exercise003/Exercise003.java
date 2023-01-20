37725f9870472f3ed2c2f3e19e1953c95e42a8eb
dwright1989 <debbiewright@live.co.uk>
2023-01-20 11:28:19 +0000

    Exercise three updated - changed == to equals

--- src/main/java/com/techreturners/exercise003/Exercise003.java
+++ src/main/java/com/techreturners/exercise003/Exercise003.java
@@ -5,7 +5,7 @@ public class Exercise003 {
     String[] iceCreamFlavours = {"Pistachio", "Raspberry Ripple", "Vanilla", "Mint Chocolate Chip", "Chocolate", "Mango Sorbet"};
     int getIceCreamCode(String iceCreamFlavour) {
         for(int i = 0; i < iceCreamFlavours.length; i++){
-            if(iceCreamFlavours[i]==iceCreamFlavour){
+            if(iceCreamFlavours[i].equals(iceCreamFlavour)){
                 return i;
             }
         }
